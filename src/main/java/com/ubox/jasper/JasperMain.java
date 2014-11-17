8eba0aaa4bf00f2e04f01e9e94c70d5440558068
awayy1432 <awayy1432@163.com>
2014-11-17 18:58:14 +0800

    初始化项目结构

---
+++ src/main/java/com/ubox/jasper/JasperMain.java
@@ -0,0 +1,14 @@
+package com.ubox.jasper;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: guojing
+ * Date: 14-11-17
+ * Time: 下午6:14
+ * To change this template use File | Settings | File Templates.
+ */
+public class JasperMain {
+    public static void main(String[] args) {
+
+    }
+}
